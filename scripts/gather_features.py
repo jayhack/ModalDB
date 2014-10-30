f2c2ca53425fe27890308a685f2ecc9ab330280c
Jay Hack <jhack@stanford.edu>
2014-10-29 21:04:02 -0700

    need to fix iter_frames'

--- scripts/gather_features.py
+++ scripts/gather_features.py
@@ -27,27 +27,36 @@ jhack@stanford.edu
 import click
 import numpy as np
 from ModalDB import *
-from schema import FrameSchema
 
-@click.option('--feature_name', 	help='Name of the feature to gather')
-@click.option('--outpath',			help='Path to store resulting matrix')
-def gather_features(feature_name, outpath):
+@click.command()
+@click.option('--feature_names', '-f', 	help='Name of the feature to gather', multiple=True)
+@click.option('--outpaths', '-o',		help='Path to store resulting matrix', multiple=True)
+def gather_features(feature_names, outpaths):
 
-	#=====[ Step 1: connect to db	]=====
-	@click.echo("---> Connecting to DB")
+	#=====[ Step 1: Sanitize input	]=====
+	if not len(feature_names) == len(outpaths):
+		click.echo("Make sure there is exactly one outpath for each feature")
+
+	#=====[ Step 1: Connect to db	]=====
+	click.echo("---> Connecting to DB")
 	db = ModalDB()
 
-	#=====[ Step 2: gather features	]=====
-	@click.echo("---> Gathering features")
-	feature_vecs = []
+	#=====[ Step 2: Gather features	]=====
+	click.echo("---> Gathering features")
+	feature_lists = {k:[] for k in feature_names}
 	for frame in db.iter_frames(verbose=True):
-		if not f['features'] is None:
-			feature_vecs.append(frame[feature_name])
+		if not any([f[k] is None for k in feature_names]):
+			for k in feature_names:
+				feature_lists[k].append(frame[k])
 
-	#=====[ Step 3: store gathered features	]=====
-	@click.echo("---> Storing gathered features")
-	feature_vecs = np.matrix(feature_vecs)
-	np.save(outpath, feature_vecs)
+	#=====[ Step 3: Store gathered features	]=====
+	click.echo("---> Storing gathered features")
+	feature_matrices = {k:np.matrix(v) for k,v in feature_lists.items()}
+	for name, path in zip(feature_names, outpaths):
+		np.save(path, feature_names[name])
+
+
+
 
 
 
@@ -57,26 +66,28 @@ def gather_features(feature_name, outpath):
 
 if __name__ == '__main__':
 
-	sd = StorageDelegate()
+	gather_features()
 
-	feature_vecs = []
-	video_ids = []	
-	frame_ids = []
-	mask_ids = []
+	# sd = StorageDelegate()
 
-	for video in sd.iter_videos(verbose=True):
-		for ix, f in enumerate(video.iter_frames(verbose=True)):
-			if ix % 25 == 0:
-				if not f.features is None:
-					feature_vecs += f.features.values()
-					video_ids += [video.name]*len(f.features.keys())
-					frame_ids += [f.index]*len(f.features.keys())
-					mask_ids += f.features.keys()
+	# feature_vecs = []
+	# video_ids = []	
+	# frame_ids = []
+	# mask_ids = []
 
-	print '=====[ ...SAVING... ]====='
-	np.save('./data/feature_vecs', np.matrix(feature_vecs))
-	np.save('./data/video_ids', np.array(video_ids))
-	np.save('./data/frame_ids', np.array(frame_ids))
-	np.save('./data/mask_ids', np.array(mask_ids))
+	# for video in sd.iter_videos(verbose=True):
+	# 	for ix, f in enumerate(video.iter_frames(verbose=True)):
+	# 		if ix % 25 == 0:
+	# 			if not f.features is None:
+	# 				feature_vecs += f.features.values()
+	# 				video_ids += [video.name]*len(f.features.keys())
+	# 				frame_ids += [f.index]*len(f.features.keys())
+	# 				mask_ids += f.features.keys()
+
+	# print '=====[ ...SAVING... ]====='
+	# np.save('./data/feature_vecs', np.matrix(feature_vecs))
+	# np.save('./data/video_ids', np.array(video_ids))
+	# np.save('./data/frame_ids', np.array(frame_ids))
+	# np.save('./data/mask_ids', np.array(mask_ids))
 	
 
