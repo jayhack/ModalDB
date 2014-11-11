82740a7455669d605433fcc9d635a62cb38b8a16
Jay Hack <jhack@stanford.edu>
2014-11-11 00:03:36 -0800

    now ModalDict tests cleaned as well

--- tests/test_ModalDicts.py
+++ tests/test_ModalDicts.py
@@ -34,11 +34,26 @@ class Test_ModalSchema(unittest.TestCase):
 	####################[ setUp	]###################################################
 	################################################################################
 
+	root = os.path.join(os.path.split(__file__)[0], 'data_ModalDicts')
+	image_path = os.path.join(root, 'image.png')
+	image_backup_path = os.path.join(root, 'image.backup.png')
+
+	def reset_image(self):
+		shutil.copy(self.image_backup_path, self.image_path)
+
+	def image_exists(self):
+		return os.path.exists(self.image_path)
+
+	def remove_image(self):
+		if self.image_exists():
+			os.remove(self.image_path)
+
+
 	def setUp(self):
 		"""
 			creates basic schema 
 		"""
-		self.root = os.path.join(os.path.split(__file__)[0], 'data_ModalDicts')
+		self.reset_image()
 		self.schema_ex = {
 							'Nesting':[Video, Frame],
 
@@ -73,9 +88,6 @@ class Test_ModalSchema(unittest.TestCase):
 									}
 								}
 					}
-		self.image_path = os.path.join(self.root, 'image.png')
-		self.image_backup_path = os.path.join(self.root, 'image.backup.png')
-		shutil.copy(self.image_backup_path, self.image_path)
 
 
 
@@ -101,6 +113,7 @@ class Test_ModalSchema(unittest.TestCase):
 			---------------------------
 			gets the image and confirms that it loaded correctly
 		"""
+		self.reset_image()
 		d = DiskDict(deepcopy(self.mongo_dict), self.schema_ex[Frame])
 		img = d['image']
 		self.assertEqual(img.shape, (512, 512, 3))
@@ -113,11 +126,12 @@ class Test_ModalSchema(unittest.TestCase):
 			gets the image, deletes it from disk, then saves it 
 			and confirms that it saved correctly
 		"""
+		self.reset_image()
 		d = DiskDict(deepcopy(self.mongo_dict), self.schema_ex[Frame])
 		img = d['image']
-		os.remove(self.image_path)
+		self.remove_image()
 		d['image'] = img
-		self.assertTrue(os.path.exists(self.image_path))
+		self.assertTrue(self.image_exists())
 
 
 	def test_delitem_diskdict(self):
@@ -126,13 +140,13 @@ class Test_ModalSchema(unittest.TestCase):
 			---------------------------
 			removes image, then replaces it 
 		"""
+		self.reset_image()
 		d = DiskDict(deepcopy(self.mongo_dict), self.schema_ex[Frame])
 		img = d['image']
 
 		del d['image']
-		self.assertTrue(not os.path.exists(self.image_path))
+		self.assertTrue(not self.image_exists())
 		self.assertEqual(d['image'], None)
-		shutil.copy(self.image_backup_path, self.image_path)
 
 
 
